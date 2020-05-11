9e1b99a691305f1920ca39ef47d2e7a6571338de
Deszip <igor.asharenkov@techery.io>
2020-05-11 17:12:50 +0300

    Dot layer layout

--- Bitrise/View/Custom/Dots view/BRDotsView.m
+++ Bitrise/View/Custom/Dots view/BRDotsView.m
@@ -11,6 +11,10 @@
 #import <QuartzCore/QuartzCore.h>
 #import "BRStyleSheet.h"
 
+@interface BRDotsView() <CALayerDelegate>
+
+@end
+
 @implementation BRDotsView
 
 - (void)drawRect:(NSRect)dirtyRect {
@@ -20,17 +24,21 @@
 - (instancetype)initWithCoder:(NSCoder *)coder {
     if (self = [super initWithCoder:coder]) {
         
+        [self setLayerContentsRedrawPolicy:NSViewLayerContentsRedrawDuringViewResize];
+        
         NSRect rect = self.frame;
         CALayer *rootLayer = [CALayer new];
         [rootLayer setFrame:rect];
+        [rootLayer setDelegate:self];
+        
         [self setLayer:rootLayer];
         [self setWantsLayer:YES];
         
+        /// Dot layer builder
         NSUInteger lineHeight = 1.0;
         NSUInteger lineSpacing = 2.0;
         NSUInteger lineLayerHeight = lineHeight + (lineSpacing * 2);
         NSUInteger lineCount = floor(rect.size.height / lineLayerHeight);
-        
         for (NSUInteger i = 0; i < lineCount; i++) {
             CALayer *lineLayer = [self buildLayerAtIndex:i height:lineHeight spacing:lineSpacing inRect:rect];
             [self.layer addSublayer:lineLayer];
@@ -40,35 +48,53 @@
     return self;
 }
 
+- (void)layoutSublayersOfLayer:(CALayer *)layer {
+    
+    NSLog(@"Frame: %f", layer.frame.size.width);
+    NSLog(@"Bounds: %f", layer.bounds.size.width);
+    NSLog(@"Sublayers: %@", layer.sublayers);
+    
+    [layer.sublayers enumerateObjectsUsingBlock:^(__kindof CALayer * _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+        [self addMask:obj];
+        NSLog(@"    Frame: %f / %f", obj.frame.size.width, obj.mask.frame.size.width);
+        NSLog(@"    Bounds: %f / %f", obj.bounds.size.width, obj.mask.bounds.size.width);
+    }];
+}
+
 - (CALayer *)buildLayerAtIndex:(NSUInteger)index height:(CGFloat)lineHeight spacing:(CGFloat)spacing inRect:(CGRect)rect {
     NSUInteger lineLayerHeight = lineHeight + (spacing * 2);
     
-    CAShapeLayer *layer = [CAShapeLayer new];
-    
-    [layer setFrame:CGRectMake(0, 0, rect.size.width, lineLayerHeight)];
-    
-    layer.strokeColor = [BRStyleSheet greenColor].CGColor;
-    layer.lineWidth = lineHeight;
-    layer.lineDashPattern = @[@(lineHeight), @(lineHeight)];
-    
-    CGMutablePathRef pathRef = CGPathCreateMutable();
-    CGPathMoveToPoint(pathRef, nil, 0, lineHeight);
-    CGPathAddLineToPoint(pathRef, nil, layer.frame.size.width, lineHeight);
-    layer.path = pathRef;
-    
     CAGradientLayer *gradientLayer = [CAGradientLayer layer];
+    gradientLayer.autoresizingMask = NSViewWidthSizable;
     gradientLayer.startPoint = CGPointMake(0.0, 1.0);
     gradientLayer.endPoint = CGPointMake(1.0, 1.0);
     gradientLayer.locations = @[@(0), @(0.2), @(0.8), @(1.0)];
-    gradientLayer.frame = CGRectMake(0, (lineLayerHeight * index) + spacing, rect.size.width, lineHeight);;
+    gradientLayer.frame = CGRectMake(0, (lineLayerHeight * index) + spacing, rect.size.width, lineHeight);
     NSArray *colors = @[ (id)[NSColor clearColor].CGColor,
                          (id)[BRStyleSheet greenColor].CGColor,
                          (id)[BRStyleSheet greenColor].CGColor,
                          (id)[NSColor clearColor].CGColor ];
     gradientLayer.colors = colors;
-    [gradientLayer setMask:layer];
-
+    
+    [self addMask:gradientLayer];
+    
     return gradientLayer;
+}
+
+- (void)addMask:(CALayer *)layer {
+    CAShapeLayer *maskLayer = [CAShapeLayer new];
+    maskLayer.contentsScale = NSScreen.mainScreen.backingScaleFactor;
+    maskLayer.frame = CGRectMake(0, 0, layer.bounds.size.width, layer.bounds.size.height);
+    maskLayer.strokeColor = [BRStyleSheet greenColor].CGColor;
+    maskLayer.lineWidth = layer.bounds.size.height;
+    maskLayer.lineDashPattern = @[@(layer.bounds.size.height), @(layer.bounds.size.height)];
+    CGMutablePathRef pathRef = CGPathCreateMutable();
+    CGPathMoveToPoint(pathRef, nil, 0, layer.bounds.size.height);
+    CGPathAddLineToPoint(pathRef, nil, layer.bounds.size.width, layer.bounds.size.height);
+    maskLayer.path = pathRef;
+    maskLayer.autoresizingMask = NSViewWidthSizable;
+    
+    layer.mask = maskLayer;
 }
 
 @end
