8a4d49ad99aabe06768dde743c0a8d7cc7096d4b
Deszip <igor.asharenkov@techery.io>
2020-06-08 20:25:29 +0300

    Top  bar layout

--- Bitrise/Storage/Data Sources/BRAppsDataSource.m
+++ Bitrise/Storage/Data Sources/BRAppsDataSource.m
@@ -87,7 +87,7 @@
 #pragma mark - NSOutlineViewDelegate -
 
 - (CGFloat)outlineView:(NSOutlineView *)outlineView heightOfRowByItem:(id)item {
-    return 116.0;
+    return 90.0;
 }
 
 - (NSTableRowView *)outlineView:(NSOutlineView *)outlineView rowViewForItem:(id)item {
