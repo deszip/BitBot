3d17fe735b96022417b96c9ed531c274250de44f
Vladislav Sosiuk <v.sosiuk@alterplay.com>
2020-10-02 17:02:44 +0300

    Fix pods

---
+++ Pods/Target Support Files/Pods-BitBot/Pods-BitBot-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_BitBot : NSObject
+@end
+@implementation PodsDummy_Pods_BitBot
+@end
---
+++ Pods/Target Support Files/Pods-BitBotTests/Pods-BitBotTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_BitBotTests : NSObject
+@end
+@implementation PodsDummy_Pods_BitBotTests
+@end
