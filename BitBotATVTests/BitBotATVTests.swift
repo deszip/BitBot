c55ebde9ddc2ad919abb3ae72904e3a4c6a653f7
Deszip <igor.asharenkov@techery.io>
2021-01-03 11:05:54 +0200

    Switch to testplans

---
+++ BitBotATVTests/BitBotATVTests.swift
@@ -0,0 +1,33 @@
+//
+//  BitBotATVTests.swift
+//  BitBotATVTests
+//
+//  Created by Deszip on 03.01.2021.
+//  Copyright © 2021 Bitrise. All rights reserved.
+//
+
+import XCTest
+
+class BitBotATVTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
