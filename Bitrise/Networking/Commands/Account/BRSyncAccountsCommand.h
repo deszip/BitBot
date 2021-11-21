dcd15b6eac882d79eb0dd238bf589bf405257f51
Deszip <raf.rafiki@gmail.com>
2021-11-21 20:18:11 +0200

    Acc sync

---
+++ Bitrise/Networking/Commands/Account/BRSyncAccountsCommand.h
@@ -0,0 +1,17 @@
+//
+//  BRSyncAccountsCommand.h
+//  BitBot
+//
+//  Created by Deszip on 21.11.2021.
+//  Copyright © 2021 Bitrise. All rights reserved.
+//
+
+#import "BRCommand.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface BRSyncAccountsCommand : BRCommand
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Bitrise/Networking/Commands/Account/BRSyncAccountsCommand.m
@@ -0,0 +1,13 @@
+//
+//  BRSyncAccountsCommand.m
+//  BitBot
+//
+//  Created by Deszip on 21.11.2021.
+//  Copyright © 2021 Bitrise. All rights reserved.
+//
+
+#import "BRSyncAccountsCommand.h"
+
+@implementation BRSyncAccountsCommand
+
+@end
