35840eca223248d02561f8e4a660adcf8be9fd3b
Deszip <raf.rafiki@gmail.com>
2022-04-17 18:17:12 +0300

    Bridge header fix

--- Bitrise/Bitrise-Bridging-Header.h
+++ Bitrise/Bitrise-Bridging-Header.h
@@ -6,7 +6,7 @@
 //  Copyright © 2020 Bitrise. All rights reserved.
 //
 
-#import "BRContainerBuilder.h"
+#import "BRPersistentContainerBuilder.h"
 #import "BRAccountsObserver.h"
 #import "BRCommand.h"
 #import "BRGetAccountCommand.h"
