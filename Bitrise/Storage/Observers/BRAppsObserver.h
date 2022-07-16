3863cabb00a2f86485f5fe62e97df8ae4fd9dfda
Deszip <raf.rafiki@gmail.com>
2022-07-16 11:42:59 +0300

    App observer api

--- Bitrise/Storage/Observers/BRAppsObserver.h
+++ Bitrise/Storage/Observers/BRAppsObserver.h
@@ -8,9 +8,30 @@
 
 #import <Foundation/Foundation.h>
 
+#import "BTRAccount+CoreDataClass.h"
+#import "BRApp+CoreDataClass.h"
+
 NS_ASSUME_NONNULL_BEGIN
 
+typedef NS_ENUM(NSUInteger, BRAppState) {
+    BRAppStateEmpty = 0,
+    BRAppStateHasData
+};
+
+typedef void(^BRAppStateCallback)(BRAppState state);
+typedef void(^BRAppUpdateCallback)(BRApp *app);
+
 @interface BRAppsObserver : NSObject
+
+@property (assign, nonatomic, readonly) BRAppState state;
+
+- (instancetype)new NS_UNAVAILABLE;
+- (instancetype)init NS_UNAVAILABLE;
+
+- (instancetype)initWithContext:(NSManagedObjectContext *)context NS_DESIGNATED_INITIALIZER;
+
+- (void)startStateObserving:(BRAppStateCallback)callback;
+- (void)startAppObserving:(NSString *)appSlug callback:(BRAppUpdateCallback)callback;
 
 @end
 
